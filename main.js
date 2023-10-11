7ddb2077e3cd11fb69a8b4d3eed33026ab79b8b6
Charlie <chrlie.w@proton.me>
2023-10-11 13:58:41 +0100

    skeleton logic

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// factory function to create player
+const Player = (name, symbol) => { 
+    return {name, symbol};
+};
+
+
+// factory function to create gameboard
+
+const Gameboard = (() => {
+
+}); 
+
+
+// game flow module
+const Game = (() => {
+
+
+});
